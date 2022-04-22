cebe0e94fa8a7f8614e2b72db3439943aa5681db
Cadeusept <matwey.likhota@gmail.com>
2022-04-22 21:03:56 +0300

    work in progress

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,6 @@
-#include <example.hpp>
+#include <boost/program_options.hpp>
+#include <hashing_db.hpp>
 
 int main() {
-  //--log-level --thread_count --output
+    //--log-level --thread_count --output
 }
--- sources/hashing_db.cpp
+++ sources/hashing_db.cpp
@@ -1,8 +1,9 @@
 // Copyright 2021 Your Name <your_email>
 
+#include <hashing_db.hpp>
 #include <stdexcept>
 
-#include <example.hpp>
+#include "../third-party/picosha2.h"
 
 auto example() -> void {
   throw std::runtime_error("not implemented");
